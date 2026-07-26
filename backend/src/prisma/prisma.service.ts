f84efea7dee87e9408ee86e48df084f39694f341
Miodrag <jankovicmiodrag2003@gmail.com>
2026-07-26 21:47:23 +0200

    PrismaService to use Postgres driver adapter and connection pool

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,13 +1,30 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client/scripts/default-index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy{
+    private static pool: Pool;
+
+    constructor() {
+        if(!PrismaService.pool) {
+            PrismaService.pool = new Pool({
+                connectionString: process.env.DATABASE_URL,
+            });
+        }
+
+        const adapter = new PrismaPg(PrismaService.pool);
+
+        super({ adapter });
+    }
+
     async onModuleInit() {
         await this.$connect();
     }
 
     async onModuleDestroy() {
         await this.$disconnect();
+        await PrismaService.pool.end();
     }
 }
